4835d7a489316a8da3bcdad603d84c0a79feaba7
arteki65 <arteki65@gmail.com>
2016-11-19 15:18:22 +0100

    add dummy offers controller and some model

---
+++ src/main/java/com/findaflat/rest/OffersController.java
@@ -0,0 +1,22 @@
+package com.findaflat.rest;
+
+import com.findaflat.model.Offer;
+import com.findaflat.model.SearchCriteria;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController("/rest/offers")
+public class OffersController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Collection<Offer>> findOffers(
+			@RequestBody
+					SearchCriteria searchCriteria) {
+		throw new UnsupportedOperationException("Non implemented yet!");
+	}
+}
