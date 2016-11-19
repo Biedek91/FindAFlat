ebdc0cec296eb054b2c455bccd57568ba9bac6b7
Biedek91 <Biedka18piotr@gmail.com>
2016-11-19 17:26:13 +0100

    Zmiana rest context

--- src/main/java/com/findaflat/rest/OffersController.java
+++ src/main/java/com/findaflat/rest/OffersController.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
-@RestController("/rest/offers")
+@RestController
+@RequestMapping("/rest/offers")
 public class OffersController {
 
 	private final OffersFinder offersFinder;
