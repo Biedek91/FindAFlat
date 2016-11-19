41200e7726c16b2da43973cf05f2f73a4f43f79d
arteki65 <arteki65@gmail.com>
2016-11-19 14:55:41 +0100

    Initial commit

---
+++ src/main/java/com/findaflat/FindAFlatApplication.java
@@ -0,0 +1,12 @@
+package com.findaflat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAFlatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAFlatApplication.class, args);
+	}
+}
